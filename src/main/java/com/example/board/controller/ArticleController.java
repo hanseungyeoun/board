092b083246fcaa4bbe429125eff0dac7201b1c7b
syhan <aksla1122@naver.com>
2023-02-25 23:04:21 +0900

    #15 - 게시판 뷰 기본 골격 작성
    뷰의 골격 잡음
    테스트에서는  media type에서 부가적인 욥션이 
    들어갈 수 있기 때문에 적절히 테스트 메소드 수정

--- src/main/java/com/example/board/controller/ArticleController.java
+++ src/main/java/com/example/board/controller/ArticleController.java
@@ -2,10 +2,21 @@ package com.example.board.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
+
+    @GetMapping
+    public String articles(ModelMap map){
+        map.addAttribute("articles", List.of());
+        return "articles/index";
+    }
+
 }
--- src/test/java/com/example/board/controller/ArticleControllerTest.java
+++ src/test/java/com/example/board/controller/ArticleControllerTest.java
@@ -23,7 +23,6 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현중")
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
