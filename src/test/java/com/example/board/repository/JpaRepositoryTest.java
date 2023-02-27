f8db3079e924adace4ad3539f8e510378d2e5c34
syhan <aksla1122@naver.com>
2023-02-27 20:12:37 +0900

    #16 - 서비스 로직 테스트 코드 업데이트 및 DTO 재설계, 쿼리 메소드 작성
    강의에서 보여주지 않은 나머지 로직의 테스트 코드를 작성하고
    연관된 DTO 를 재설계함
    테스트가 필요로 하는 repository 쿼리 메소드도 작성

--- src/test/java/com/example/board/repository/JpaRepositoryTest.java
+++ src/test/java/com/example/board/repository/JpaRepositoryTest.java
@@ -2,38 +2,45 @@ package com.example.board.repository;
 
 import com.example.board.config.JpaConfig;
 import com.example.board.domain.Article;
-import org.assertj.core.api.Assertions;
+import com.example.board.domain.UserAccount;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
-@Import(JpaConfig.class)
 @DisplayName("JPA 연결 테스트")
+@Import(JpaConfig.class)
+@DataJpaTest
 class JpaRepositoryTest {
 
-    private final ArticleCommentRepository articleCommentRepository;
     private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+    private final UserAccountRepository userAccountRepository;
 
     public JpaRepositoryTest(
+            @Autowired ArticleRepository articleRepository,
             @Autowired ArticleCommentRepository articleCommentRepository,
-            @Autowired ArticleRepository articleRepository) {
-        this.articleCommentRepository = articleCommentRepository;
+            @Autowired UserAccountRepository userAccountRepository
+    ) {
         this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+        this.userAccountRepository = userAccountRepository;
     }
 
     @DisplayName("select 테스트")
     @Test
-    void givenTestData_whenSelecting_thenWorkFine() {
+    void givenTestData_whenSelecting_thenWorksFine() {
+        // Given
 
+        // When
         List<Article> articles = articleRepository.findAll();
+
+        // Then
         assertThat(articles)
                 .isNotNull()
                 .hasSize(123);
@@ -41,14 +48,17 @@ class JpaRepositoryTest {
 
     @DisplayName("insert 테스트")
     @Test
-    void givenTestData_whenInserting_thenWorkFine() {
-        long prevCount = articleRepository.count();
+    void givenTestData_whenInserting_thenWorksFine() {
+        // Given
+        long previousCount = articleRepository.count();
+        UserAccount userAccount = userAccountRepository.save(UserAccount.of("uno", "pw", null, null, null));
+        Article article = Article.of(userAccount, "new article", "new content", "#spring");
 
-        Article savedArticle = articleRepository.save(Article.of("new title", "new content", "#spring"));
+        // When
+        articleRepository.save(article);
 
-        List<Article> articles = articleRepository.findAll();
-        assertThat(articleRepository.count())
-                .isEqualTo(prevCount + 1);
+        // Then
+        assertThat(articleRepository.count()).isEqualTo(previousCount + 1);
     }
 
     @DisplayName("update 테스트")
@@ -56,12 +66,14 @@ class JpaRepositoryTest {
     void givenTestData_whenUpdating_thenWorksFine() {
         // Given
         Article article = articleRepository.findById(1L).orElseThrow();
-        String updateHashtag = "#springboot";
-        article.setHashtag(updateHashtag);
+        String updatedHashtag = "#springboot";
+        article.setHashtag(updatedHashtag);
 
+        // When
         Article savedArticle = articleRepository.saveAndFlush(article);
 
-        assertThat(savedArticle).hasFieldOrPropertyWithValue("hashtag", updateHashtag);
+        // Then
+        assertThat(savedArticle).hasFieldOrPropertyWithValue("hashtag", updatedHashtag);
     }
 
     @DisplayName("delete 테스트")
@@ -71,12 +83,14 @@ class JpaRepositoryTest {
         Article article = articleRepository.findById(1L).orElseThrow();
         long previousArticleCount = articleRepository.count();
         long previousArticleCommentCount = articleCommentRepository.count();
-        int deleteCommentSize = article.getArticleComments().size();
+        int deletedCommentsSize = article.getArticleComments().size();
 
+        // When
         articleRepository.delete(article);
 
-
+        // Then
         assertThat(articleRepository.count()).isEqualTo(previousArticleCount - 1);
-        assertThat(articleCommentRepository.count()).isEqualTo(previousArticleCommentCount - deleteCommentSize);
+        assertThat(articleCommentRepository.count()).isEqualTo(previousArticleCommentCount - deletedCommentsSize);
     }
+
 }
