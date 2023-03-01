323e1e605d81a1063acb3ed8d8db586865e10c76
syhan <aksla1122@naver.com>
2023-03-01 21:58:45 +0900

    #21 - 페이지 네이션 기능을 Controller (게시판 뷰)에 적용

--- src/main/java/com/example/board/controller/ArticleController.java
+++ src/main/java/com/example/board/controller/ArticleController.java
@@ -4,6 +4,7 @@ import com.example.board.domain.type.SearchType;
 import com.example.board.dto.reponse.ArticleResponse;
 import com.example.board.dto.reponse.ArticleWithCommentsResponse;
 import com.example.board.service.ArticleService;
+import com.example.board.service.PaginationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,12 +17,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
 
     private final ArticleService articleService;
+    private final PaginationService paginationService;
 
     @GetMapping
     public String articles(
@@ -31,7 +35,11 @@ public class ArticleController {
             ModelMap map
     ) {
         Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+
         map.addAttribute("articles", articles);
+        map.addAttribute("paginationBarNumbers", barNumbers);
+
         return "articles/index";
     }
 
